8c3d0e5fb28357461cf70de2e83958ff12d5d73d
suja <mahrjansuja222@gmail.com>
2024-09-01 21:09:41 +0545

    contact modifiction

--- src/components/contact_components/ContactusForm.js
+++ src/components/contact_components/ContactusForm.js
@@ -4,73 +4,65 @@ import '../../css/contactus.css'
 const ContactusForm = () => {
     return (
         <div className='' style={{ backgroundImage: "linear-gradient(180deg, #FFFFFF 0%, #D4E4F4 100%)" }}>
-            <div className="contactuscontainer">
-                <div className="clientsection-info">
-                    <div className="conatctnumber">
-                        <h2>We are here to help!</h2>
-                        <h5>Panga, Kirtipur</h5>
-                        <h5>  <i class="bi bi-telephone-fill"></i>985414425142</h5>
-                        <h5> <i class="bi bi-telephone-fill"></i>015454654</h5>
+            <section className='container'>
+                <div className="row contactuscontainer ps-2">
+                    <div className="col-md-6">
+                        <div className="conatctnumber">
+                            <h2 className='contact-heading pt-5'>We are here to help!</h2>
+                            <p><i class="bi bi-geo-alt-fill pe-3"></i>Sabhagriha, Pokhara-8</p>
+                            <p><i class="bi bi-telephone-fill pe-3"></i>985414425142</p>
+                            <p> <i class="bi bi-telephone-fill pe-3"></i>015454654</p>
+
+                        </div>
+                        <div className="social-account mt-5">
+                            <p className='form-heading'>Follow Us</p>
+                            <div className='d-flex media-icon'>
+                            <div className='text-center'><i class="bi bi-facebook"></i></div>
+                            <div className='mx-5'><i class="bi bi-instagram"></i></div>
+                            </div>
+                           
+
+                        </div>
 
                     </div>
-                    <div className="social-account">
-                        <div className='text-center'><i class="bi bi-facebook"></i></div>
-                        <div className='mx-5'><i class="bi bi-instagram"></i></div>
+                    <div className="col-md-6 clientsectionform pb-5" style={{ backgroundColor: 'white'}}>
+                        <form className='ps-3'>
+                            <h3 className='form-heading pt-5'>Send Us Message</h3>
+                            <div class="form-row">
+                                <div class="row g-2">
+                                    <div class="form-group col-6">
+                                        <input type="email" class="form-control" id="inputEmail4" placeholder="First Name" />
 
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <input type="email" class="form-control" id="inputEmail4" placeholder="Last Name" />
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+                            <div class="form-group">
+                               
+                                <input type="text" class="form-control" id="inputAddress" placeholder="Email Address" />
+                            </div>
+                            <div class="form-group">
+
+                                <input type="text" class="form-control" id="inputAddress2" placeholder="Subject" />
+                            </div>
+                            <div class="form-floating">
+                                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style={{height:"100px",resize: "none"}}></textarea>
+                                <label for="floatingTextarea">Your Message</label>
+                            </div>
+
+
+                            <button type="submit" class="btn submit-btn mt-4 px-4 py-2">Send Message</button>
+                        </form>
                     </div>
+                </div>
 
-                </div>
-                <div className="clientsectionform" style={{ backgroundColor: 'white' }}>
-                    <form>
-                        <h2></h2>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputEmail4">First Name</label>
-                                <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
-                                
-                           </div>
-                           <div class="form-group col-md-6">
-                           <label for="inputPassword4">Last Name</label>
-                           <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
-                           </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputAddress">Address</label>
-                            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
-                        </div>
-                        <div class="form-group">
-                            <label for="inputAddress2">Address 2</label>
-                            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputCity">City</label>
-                                <input type="text" class="form-control" id="inputCity" />
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label for="inputState">State</label>
-                                <select id="inputState" class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-md-2">
-                                <label for="inputZip">Zip</label>
-                                <input type="text" class="form-control" id="inputZip" />
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" id="gridCheck" />
-                                <label class="form-check-label" for="gridCheck">
-                                    Check me out
-                                </label>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Sign in</button>
-                    </form>
-                </div>
-            </div>
+            </section>
+
         </div>
     )
 }
